c52e101d683a23311d1518b4c32c28fabdcf0a1b
ChenESia <cheneus@gmail.com>
2018-01-09 20:41:55 -0600

    Initial commit

---
+++ app/routing/apiRouter.js
@@ -0,0 +1,61 @@
+console.log("apiRoute R");
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const apiRouter = express.Router();
+
+apiRouter.route('/')
+// function to be used
+.get((req, res, next) => {
+  // connection.query("SELECT * FROM books;", (err, data) => {
+  //   if (err) throw err;
+  //   console.log(data)
+  //   // Test it
+  //   // console.log('The solution is: ', data);
+
+  //   // Test it
+  //   // return res.send(data);
+
+  //   res.render("index", { burgers: data });
+  // });
+  console.log("apiget")
+      res.render("index", { burgers: data });
+})
+ .put((req, res, next) => {
+  console.log("apiput")
+ })
+
+// Post route -> back to home
+.post((req, res)=> {
+  // Test it
+  // console.log('You sent, ' + req.body.task);
+  console.log("req.body", req.body)
+  console.log(req.body.burger_name)
+
+  // Test it
+  // return res.send('You sent, ' + req.body.task);
+
+  // connection.query("INSERT INTO burgers (burger_name) VALUES (?)", [req.body.burger_name], function(err, result) {
+  //   if (err) throw err;
+  // });
+  res.redirect("/");
+});
+
+// // app.delete("/", (req, res) => {
+// //   connection.query("")
+// // })
+
+// app.put("/update", (req, res) => {
+//   // UPDATE `burgers_db`.`burgers` SET `devoured`='1' WHERE `id`='1';
+//   console.log(req.body.id)
+//   console.log("MKAE")
+//   connection.query("UPDATE burgers_db.burgers SET devoured=1 WHERE id=?", [req.body.id], function(err, result) {
+//     if (err) throw err;
+//   })
+//   res.redirect(303, '/')
+// })
+// ======================================
+
+module.exports = apiRouter
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 8080;
+
+// routing 
+const apiRouter = require('./app/routing/apiRouter.js')
+const htmlRouter = require('./app/routing/htmlRouter.js')
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "handlebars");
+// app.use(express.static('js'));
+
+app.use(express.static('app/public'));
+
+app.use('/', htmlRouter)
+app.use('/api', apiRouter)
+
+
+app.listen(port, () => {
+  console.log("App listening on PORT " + port);
+
+});
